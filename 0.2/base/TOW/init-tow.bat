8866f06e72b6327a6deab30f316bc8d51a7cd546
yeoupooh <yeoupooh@38da483d-063f-0410-804a-af7c5303fc05>
2007-12-12 07:31:56 +0000

    - added apache log files.

--- 0.2/base/TOW/init-tow.bat
+++ 0.2/base/TOW/init-tow.bat
@@ -4,7 +4,11 @@ call set-tow.bat
 
 if not "%1"=="start" goto usage
 
+copy %TOW_SETUP_HOME%\README-%TOW_LANG%.txt %TOW_HOME%\README.txt
+
 copy %TOW_SETUP_HOME%\Apache\conf\httpd-%TOW_LANG%.conf %APACHE_HOME%\conf\httpd.conf
+copy %TOW_SETUP_HOME%\Apache\logs\*.* %APACHE_HOME%\logs
+
 copy %TOW_SETUP_HOME%\Python\share\trac\wiki-default\WikiStart-%TOW_LANG% %PYTHON_HOME%\share\trac\wiki-default\WikiStart
 
 if exist "%TOW_SVN_PRJ%\%TOW_TEST_PROJECT%" rd /S /Q "%TOW_SVN_PRJ%\%TOW_TEST_PROJECT%"
