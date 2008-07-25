4908fdeb4680fb888ad3a83ab390a6fea0dfc78e
yeoupooh <yeoupooh@38da483d-063f-0410-804a-af7c5303fc05>
2008-07-25 09:26:30 +0000

    * recovered 0.2 files.

--- trunk/0.2/base/TOW/create-trac-repo.bat
+++ trunk/0.2/base/TOW/create-trac-repo.bat
@@ -5,37 +5,30 @@ call set-tow.bat
 if "%1"=="" goto usage
 
 rem trac-admin
-rem initenv <projectname> <db> <repostype> <repospath>
+rem initenv <projectname> <db> <repostype> <repospath> <templatepath>
 rem         -- Create and initialize a new environment from arguments
 
-rem set environment variables
 set _PROJECTNAME=%1
 set _DB=sqlite:db/trac.db
 set _REPOSTYPE=svn
 set _REPOSPATH="%TOW_SVN_PRJ%\%1"
+set _TEMPLATEPATH="%PYTHON_HOME%\share\trac\templates"
 
-rem display environment variables
 echo PROJECTNAME=%_PROJECTNAME%
 echo DB=%_DB%
 echo REPOSTYPE=%_REPOSTYPE%
 echo REPOSPATH=%_REPOSPATH%
+echo TEMPLATEPATH=%_TEMPLATEPATH%
 
-rem init trac environent
-call trac-admin %1 initenv %_PROJECTNAME% %_DB% %_REPOSTYPE% %_REPOSPATH%
+%PYTHON_HOME%\python %PYTHON_HOME%\Scripts\trac-admin "%TOW_TRAC_PRJ%\%1" initenv %_PROJECTNAME% %_DB% %_REPOSTYPE% %_REPOSPATH% %_TEMPLATEPATH%
 call lang TRAC_REPO_CREATED %TOW_SVN_PRJ%\%1
 
-rem copy trac.ini file
-start %PYTHON_HOME%\python rep-copy.py %_PROJECTNAME% %TOW_SETUP_HOME%\TracRepo\Projects\%TOW_TEST_PROJECT%\conf\trac-%TOW_PACKAGE%-%TOW_LANG%.ini.tpl "%TOW_TRAC_PRJ%\%1\conf\trac.ini"
+%PYTHON_HOME%\python rep-copy.py %_PROJECTNAME% %TOW_SETUP_HOME%\TracRepo\Projects\%TOW_TEST_PROJECT%\conf\trac-%TOW_PACKAGE%-%TOW_LANG%.ini.tpl "%TOW_TRAC_PRJ%\%1\conf\trac.ini"
 call lang TRAC_INI_COPIED
 
-rem copy logo image
-copy "%TOW_SETUP_HOME%\TracRepo\Projects\%TOW_TEST_PROJECT%\htdocs\tow-logo.png" "%TOW_TRAC_PRJ%\%1\htdocs"
-call lang TRAC_BANNER_COPIED
-
-rem grant permissions to admin account
 if "%TOW_PACKAGE%"=="base" goto end
-trac-admin "%TOW_TRAC_PRJ%\%1" permission add admin TRAC_ADMIN
-trac-admin "%TOW_TRAC_PRJ%\%1" permission add admin XML_RPC
+%PYTHON_HOME%\python %PYTHON_HOME%\Scripts\trac-admin "%TOW_TRAC_PRJ%\%1" permission add admin TRAC_ADMIN
+%PYTHON_HOME%\python %PYTHON_HOME%\Scripts\trac-admin "%TOW_TRAC_PRJ%\%1" permission add admin XML_RPC
 call lang TRAC_ADMIN_SET
 
 goto end
