b59712b4f1f6e368568ae4794108776dd9b8b9f1
yeoupooh <yeoupooh@38da483d-063f-0410-804a-af7c5303fc05>
2008-08-14 02:54:21 +0000

    * fixed svn fstype param for svn 1.4 client.  * fixed httpd.conf in authz.  * added svn-python.

--- trunk/0.3/base/TOW/create-svn-repo.bat
+++ trunk/0.3/base/TOW/create-svn-repo.bat
@@ -4,7 +4,8 @@ call set-tow.bat
 
 if "%1"=="" goto usage
 
-"%SVN_HOME%\bin\svnadmin" create --fs-type=fsfs "%TOW_SVN_PRJ%\%1"
+rem --pre-1.4-compatible for svn 1.4 client
+"%SVN_HOME%\bin\svnadmin" create --pre-1.4-compatible --fs-type fsfs "%TOW_SVN_PRJ%\%1"
 call lang SVN_REPO_CREATED "%TOW_SVN_PRJ%\%1"
 
 "%SVN_HOME%\bin\svn" import %TOW_HOME%\SvnTemplate file:///C:/TOW/SvnRepo/Projects/%1 -m "initial import"
